774d2874969e675e5aaa90c2c47c435896b352e5
Trexter <sheridak@purdue.edu>
2016-11-07 21:53:18 -0500

    started state transition

--- include/pauvsi_vio/VIOEKF.cpp
+++ include/pauvsi_vio/VIOEKF.cpp
@@ -32,13 +32,29 @@ VIOState VIOEKF::transitionState(VIOState x, sensor_msgs::Imu imu, double dt)
 	}
 
 	//convert the imu readings to tf::Vectors and remove their biases
-	tf::Vector3 alpha(imu.linear_acceleration.x, imu.linear_acceleration.y, imu.linear_acceleration.z);
-	alpha = this->scaleAccelerometer * alpha;
-	tf::Vector3 omega(imu.angular_velocity.x - this->gyroBiasX, imu.angular_velocity.y - this->gyroBiasY, imu.angular_velocity.z - this->gyroBiasZ);
+	tf::Vector3 alpha_tf(imu.linear_acceleration.x, imu.linear_acceleration.y, imu.linear_acceleration.z);
+	alpha_tf = this->scaleAccelerometer * alpha_tf;
+	tf::Vector3 omega_tf(imu.angular_velocity.x - this->gyroBiasX, imu.angular_velocity.y - this->gyroBiasY, imu.angular_velocity.z - this->gyroBiasZ);
 
 	//transform the imu readings into the center of mass frame
-	alpha = imu2odom * alpha - imu2odom * tf::Vector3(0.0, 0.0, 0.0);
-	alpha = imu2odom * omega - imu2odom * tf::Vector3(0.0, 0.0, 0.0);
+	alpha_tf = imu2odom * alpha_tf - imu2odom * tf::Vector3(0.0, 0.0, 0.0);
+	omega_tf = imu2odom * omega_tf - imu2odom * tf::Vector3(0.0, 0.0, 0.0);
+
+	Eigen::Vector3d alpha, omega;
+	alpha << alpha_tf.getX(), alpha_tf.getY(), alpha_tf.getZ();
+	omega << omega_tf.getX(), omega_tf.getY(), omega_tf.getZ();
+
+	VIOState xNew;
+	double ax = (alpha(0) * (1 - 2*x.q2()*x.q2() - 2*x.q3()*x.q3()) +
+			alpha(1) * 2 * (x.q1()*x.q2() + x.q0()*x.q3()) + alpha(2) * 2 * (x.q1()*x.q3() - x.q0()*x.q2()));
+	double ay = (alpha(0)*2*(x.q1()*x.q2() - x.q0()*x.q3()) + alpha(1)*(1-2*x.q1()*x.q1() - 2*x.q3()*x.q3())
+			+ alpha(2)*2*(x.q2()*x.q3() + x.q0()*x.q1()));
+
+
+	//transition state
+	xNew.vector(0, 0) = x.x() + x.dx()*dt + 0.5 * ax * dt*dt;
+
+	xNew.vector(1, 0) = x.y() + x.dy()*dt + 0.5 * ay * dt*dt;
 }
 
 
