ea0524d8256eb400b09c0447b8db053f02d049cc
Trexter <sheridak@purdue.edu>
2016-11-16 22:26:55 -0500

    implemented the state transition jacobian function

--- include/pauvsi_vio/VIOEKF.cpp
+++ include/pauvsi_vio/VIOEKF.cpp
@@ -72,7 +72,7 @@ VIOState VIOEKF::transitionState(VIOState x, double dt)
 	double dq1 = 0;
 	double dq2 = 0;
 	double dq3 = 0;
-	
+
 	if(w_mag != 0)
 	{
 		dq0 = cos(0.5 * w_mag * dt);
@@ -125,27 +125,27 @@ VIOState VIOEKF::transitionState(VIOState x, double dt)
 /*
  * this constructs the jacobain of the state transiion function
  */
-Eigen::Matrix<double, 16, 16> stateJacobian(VIOState x, double dt){
+Eigen::Matrix<double, 16, 16> stateJacobian(VIOState state, double dt){
 
 	Eigen::Matrix<double, 16, 16> H;
 
-	double x = x.x();
-	double y = x.y();
-	double z = x.z();
-	double dx = x.dx();
-	double dy = x.dy();
-	double dz = x.dz();
-	double q0 = x.q0();
-	double q1 = x.q1();
-	double q2 = x.q2();
-	double q3 = x.q3();
-	double ax = x.getAlpha()(0);
-	double ay = x.getAlpha()(1);
-	double az = x.getAlpha()(2);
-	double wx = x.getOmega()(0);
-	double wy = x.getOmega()(1);
-	double wz = x.getOmega()(2);
-	double omegaMag = x.getOmega().norm();
+	double x = state.x();
+	double y = state.y();
+	double z = state.z();
+	double dx = state.dx();
+	double dy = state.dy();
+	double dz = state.dz();
+	double q0 = state.q0();
+	double q1 = state.q1();
+	double q2 = state.q2();
+	double q3 = state.q3();
+	double ax = state.getAlpha()(0);
+	double ay = state.getAlpha()(1);
+	double az = state.getAlpha()(2);
+	double wx = state.getOmega()(0);
+	double wy = state.getOmega()(1);
+	double wz = state.getOmega()(2);
+	double omegaMag = state.getOmega().norm();
 
 	//handle the two cases for omega
 	if(omegaMag > 0.0)
@@ -158,25 +158,25 @@ Eigen::Matrix<double, 16, 16> stateJacobian(VIOState x, double dt){
 				4*q3*wy*wy*cs1*cs1*cs1 + 4*q3*wz*wz*cs1*cs1*cs1 - dt*q2*wy*wy*wy*cs1 + dt*q1*wy*wy*wy*cs1 -
 				dt*q0*wy*wy*wy*cs1 + dt*q1*wx*wx*wy*cs1 - dt*q2*wx*wy*wy*cs1 - dt*q0*wx*wx*wz*cs1 -
 				dt*q2*wx*wz*wz*cs1 - dt*q0*wy*wy*wz*cs1 + dt*q1*wy*wz*wz*cs1 + 2*dt*q3*s2*wx*wx*ss1 +
-				2*dt*q3*s2*wy*wy*ss1 + 2*dt*q3*s2*wz*wz*ss1 + 2*q2*s2*wx*cs1^2*ss1 - 2*q1*s2*wy*cs1^2*ss1 + 2*q0*s2*wz*cs1^2*ss1;
+				2*dt*q3*s2*wy*wy*ss1 + 2*dt*q3*s2*wz*wz*ss1 + 2*q2*s2*wx*cs1*cs1*ss1 - 2*q1*s2*wy*cs1*cs1*ss1 + 2*q0*s2*wz*cs1*cs1*ss1;
 		double s4 = 4*q2*wx*wx*cs1*cs1*cs1 - 4*q2*wy*wy*cs1 - 4*q2*wz*wz*cs1 - 4*q2*wx*wx*cs1 +
-				4*q2*wy*wy*cs1*cs1*cs1 + 4*q2*wz*wz*cs1*cs1*cs1 + dt*q3*wx^3*cs1 - dt*q0*wy^3*cs1 -
+				4*q2*wy*wy*cs1*cs1*cs1 + 4*q2*wz*wz*cs1*cs1*cs1 + dt*q3*wx*wx*wx*cs1 - dt*q0*wy*wy*wy*cs1 -
 				dt*q1*wz*wz*wz*cs1 - dt*q0*wx*wx*wy*cs1 + dt*q3*wx*wy*wy*cs1 - dt*q1*wx*wx*wz*cs1 +
 				dt*q3*wx*wz*wz*cs1 - dt*q0*wy*wz*wz*cs1 - dt*q1*wy*wy*wz*cs1 +
 				2*dt*q2*s2*wx*wx*ss1 + 2*dt*q2*s2*wy*wy*ss1 + 2*dt*q2*s2*wz*wz*ss1 -
-				2*q3*s2*wx*cs1^2*ss1 + 2*q0*s2*wy*cs1^2*ss1 + 2*q1*s2*wz*cs1^2*ss1;
+				2*q3*s2*wx*cs1*cs1*ss1 + 2*q0*s2*wy*cs1*cs1*ss1 + 2*q1*s2*wz*cs1*cs1*ss1;
 		double s5 = 4*q1*wx*wx*cs1*cs1*cs1 - 4*q1*wy*wy*cs1 - 4*q1*wz*wz*cs1 - 4*q1*wx*wx*cs1 +
-				4*q1*wy*wy*cs1*cs1*cs1 + 4*q1*wz*wz*cs1*cs1*cs1 - dt*q0*wx^3*cs1 - dt*q3*wy^3*cs1 +
+				4*q1*wy*wy*cs1*cs1*cs1 + 4*q1*wz*wz*cs1*cs1*cs1 - dt*q0*wx*wx*wx*cs1 - dt*q3*wy*wy*wy*cs1 +
 				dt*q2*wz*wz*wz*cs1 - dt*q0*wx*wy*wy*cs1 - dt*q3*wx*wx*wy*cs1 - dt*q0*wx*wz*wz*cs1 +
 				dt*q2*wx*wx*wz*cs1 + dt*q2*wy*wy*wz*cs1 - dt*q3*wy*wz*wz*cs1 + 2*dt*q1*s2*wx*wx*ss1 +
-				2*dt*q1*s2*wy*wy*ss1 + 2*dt*q1*s2*wz*wz*ss1 + 2*q0*s2*wx*cs1^2*ss1 +
-				2*q3*s2*wy*cs1^2*ss1 - 2*q2*s2*wz*cs1^2*ss1;
+				2*dt*q1*s2*wy*wy*ss1 + 2*dt*q1*s2*wz*wz*ss1 + 2*q0*s2*wx*cs1*cs1*ss1 +
+				2*q3*s2*wy*cs1*cs1*ss1 - 2*q2*s2*wz*cs1*cs1*ss1;
 		double s6 = 4*q0*wx*wx*cs1*cs1*cs1 - 4*q0*wy*wy*cs1 - 4*q0*wz*wz*cs1 - 4*q0*wx*wx*cs1 +
-				4*q0*wy*wy*cs1*cs1*cs1 + 4*q0*wz*wz*cs1*cs1*cs1 + dt*q1*wx^3*cs1 + dt*q2*wy^3*cs1 +
+				4*q0*wy*wy*cs1*cs1*cs1 + 4*q0*wz*wz*cs1*cs1*cs1 + dt*q1*wx*wx*wx*cs1 + dt*q2*wy*wy*wy*cs1 +
 				dt*q3*wz*wz*wz*cs1 + dt*q1*wx*wy*wy*cs1 + dt*q2*wx*wx*wy*cs1 + dt*q1*wx*wz*wz*cs1 +
 				dt*q3*wx*wx*wz*cs1 + dt*q2*wy*wz*wz*cs1 + dt*q3*wy*wy*wz*cs1 +
 				2*dt*q0*s2*wx*wx*ss1 + 2*dt*q0*s2*wy*wy*ss1 + 2*dt*q0*s2*wz*wz*ss1 -
-				2*q1*s2*wx*cs1^2*ss1 - 2*q2*s2*wy*cs1^2*ss1 - 2*q3*s2*wz*cs1^2*ss1;
+				2*q1*s2*wx*cs1*cs1*ss1 - 2*q2*s2*wy*cs1*cs1*ss1 - 2*q3*s2*wz*cs1*cs1*ss1;
 		double s7 = 5 - 3*cos(dt*s2);
 		double s8 = sqrt(2);
 		double s9 = 1/sqrt(s7);
@@ -206,6 +206,7 @@ Eigen::Matrix<double, 16, 16> stateJacobian(VIOState x, double dt){
 		double s33 = 2*az*q0;
 		double s34 = 2*s25;
 		double s35 = 2*s24;
+		double s36 = 2*s22;
 		double s37 = 2*s26;
 		double s38 = ay*q2;
 		double s39 = ax*q1;
@@ -215,25 +216,26 @@ Eigen::Matrix<double, 16, 16> stateJacobian(VIOState x, double dt){
 		double s43 = ax*q2;
 
 
-		H << 1, 0, 0, dt,  0,  0,  s10*(s40 - az*q2),           s10*(s38 + az*q3),  -s10*(s21 - ay*q1 + az*q0),     s10*(s22 + s42 - ax*q3),                      0,                      0,                      0, -s10*(s24 + s26 - 1/2),    s10*(q0*q3 + q1*q2),     -s10*(s27 - q1*q3)
-				 0, 1, 0,  0, dt,  0,            s10*s22, s10*(s43 - 2*ay*q1 + az*q0),           s10*(s39 + az*q3),    -s10*(s23 + s41 - az*q2),                      0,                      0,                      0,               -s10*s18, -s10*(s25 + s26 - 1/2),    s10*(q0*q1 + q2*q3)
-				 0, 0, 1,  0,  0, dt,  s10*(s43 - ay*q1),      -s10*(s20 + s22 + s42),   s10*(s40 + s41 - 2*az*q2),             s10*(s38 + s39),                      0,                      0,                      0,      s10*(s27 + q1*q3),               -s10*s17, -s10*(s24 + s25 - 1/2)
-				 0, 0, 0,  1,  0,  0, dt*(s23 - 2*az*q2),              dt*(s28 + s32), -dt*(s33 + 4*s43 - 2*ay*q1),    dt*(s31 + s36 - 2*ax*q3),                      0,                      0,                      0,    -dt*(s35 + s37 - 1), dt*(2*q0*q3 + 2*q1*q2),    -2*dt*(s27 - q1*q3)
-				 0, 0, 0,  0,  1,  0,             dt*s36,    dt*(s21 + s33 - 4*ay*q1),              dt*(s29 + s32), -dt*(s30 + 4*s40 - 2*az*q2),                      0,                      0,                      0,              -2*dt*s18,    -dt*(s34 + s37 - 1), dt*(2*q0*q1 + 2*q2*q3)
-				 0, 0, 0,  0,  0,  1, dt*(s21 - 2*ay*q1),     -dt*(2*s20 + s31 + s36),    dt*(s23 + s30 - 4*az*q2),              dt*(s28 + s29),                      0,                      0,                      0,   dt*(2*s27 + 2*q1*q3),              -2*dt*s17,    -dt*(s34 + s35 - 1)
-				 0, 0, 0,  0,  0,  0,                s15,                        -s13,                        -s14,                        -s19, -2*s6*s8*s9*s11*s16*wx, -2*s6*s8*s9*s11*s16*wy, -2*s6*s8*s9*s11*s16*wz,                      0,                      0,                      0
-				 0, 0, 0,  0,  0,  0,                s13,                         s15,                        -s19,                         s14, -2*s5*s8*s9*s11*s16*wx, -2*s5*s8*s9*s11*s16*wy, -2*s5*s8*s9*s11*s16*wz,                      0,                      0,                      0
-				 0, 0, 0,  0,  0,  0,                s14,                         s19,                         s15,                        -s13, -2*s4*s8*s9*s11*s16*wx, -2*s4*s8*s9*s11*s16*wy, -2*s4*s8*s9*s11*s16*wz,                      0,                      0,                      0
-				 0, 0, 0,  0,  0,  0,                s19,                        -s14,                         s13,                         s15, -2*s3*s8*s9*s11*s16*wx, -2*s3*s8*s9*s11*s16*wy, -2*s3*s8*s9*s11*s16*wz,                      0,                      0,                      0
-				 0, 0, 0,  0,  0,  0,                  0,                           0,                           0,                           0,                      1,                      0,                      0,                      0,                      0,                      0
-				 0, 0, 0,  0,  0,  0,                  0,                           0,                           0,                           0,                      0,                      1,                      0,                      0,                      0,                      0
-				 0, 0, 0,  0,  0,  0,                  0,                           0,                           0,                           0,                      0,                      0,                      1,                      0,                      0,                      0
+		H << 1, 0, 0, dt,  0,  0,  s10*(s40 - az*q2),           s10*(s38 + az*q3),  -s10*(s21 - ay*q1 + az*q0),     s10*(s22 + s42 - ax*q3),                      0,                      0,                      0, -s10*(s24 + s26 - 1/2),    s10*(q0*q3 + q1*q2),     -s10*(s27 - q1*q3),
+				0, 1, 0,  0, dt,  0,            s10*s22, s10*(s43 - 2*ay*q1 + az*q0),           s10*(s39 + az*q3),    -s10*(s23 + s41 - az*q2),                      0,                      0,                      0,               -s10*s18, -s10*(s25 + s26 - 1/2),    s10*(q0*q1 + q2*q3),
+				0, 0, 1,  0,  0, dt,  s10*(s43 - ay*q1),      -s10*(s20 + s22 + s42),   s10*(s40 + s41 - 2*az*q2),             s10*(s38 + s39),                      0,                      0,                      0,      s10*(s27 + q1*q3),               -s10*s17, -s10*(s24 + s25 - 1/2),
+				0, 0, 0,  1,  0,  0, dt*(s23 - 2*az*q2),              dt*(s28 + s32), -dt*(s33 + 4*s43 - 2*ay*q1),    dt*(s31 + s36 - 2*ax*q3),                      0,                      0,                      0,    -dt*(s35 + s37 - 1), dt*(2*q0*q3 + 2*q1*q2),    -2*dt*(s27 - q1*q3),
+				0, 0, 0,  0,  1,  0,             dt*s36,    dt*(s21 + s33 - 4*ay*q1),              dt*(s29 + s32), -dt*(s30 + 4*s40 - 2*az*q2),                      0,                      0,                      0,              -2*dt*s18,    -dt*(s34 + s37 - 1), dt*(2*q0*q1 + 2*q2*q3),
+				0, 0, 0,  0,  0,  1, dt*(s21 - 2*ay*q1),     -dt*(2*s20 + s31 + s36),    dt*(s23 + s30 - 4*az*q2),              dt*(s28 + s29),                      0,                      0,                      0,   dt*(2*s27 + 2*q1*q3),              -2*dt*s17,    -dt*(s34 + s35 - 1),
+				0, 0, 0,  0,  0,  0,                s15,                        -s13,                        -s14,                        -s19, -2*s6*s8*s9*s11*s16*wx, -2*s6*s8*s9*s11*s16*wy, -2*s6*s8*s9*s11*s16*wz,                      0,                      0,                      0,
+				0, 0, 0,  0,  0,  0,                s13,                         s15,                        -s19,                         s14, -2*s5*s8*s9*s11*s16*wx, -2*s5*s8*s9*s11*s16*wy, -2*s5*s8*s9*s11*s16*wz,                      0,                      0,                      0,
+				0, 0, 0,  0,  0,  0,                s14,                         s19,                         s15,                        -s13, -2*s4*s8*s9*s11*s16*wx, -2*s4*s8*s9*s11*s16*wy, -2*s4*s8*s9*s11*s16*wz,                      0,                      0,                      0,
+				0, 0, 0,  0,  0,  0,                s19,                        -s14,                         s13,                         s15, -2*s3*s8*s9*s11*s16*wx, -2*s3*s8*s9*s11*s16*wy, -2*s3*s8*s9*s11*s16*wz,                      0,                      0,                      0,
+				0, 0, 0,  0,  0,  0,                  0,                           0,                           0,                           0,                      1,                      0,                      0,                      0,                      0,                      0,
+				0, 0, 0,  0,  0,  0,                  0,                           0,                           0,                           0,                      0,                      1,                      0,                      0,                      0,                      0,
+				0, 0, 0,  0,  0,  0,                  0,                           0,                           0,                           0,                      0,                      0,                      1,                      0,                      0,                      0;
 	}
 	else
 	{
-
+		H = Eigen::MatrixXd::Identity(16, 16); // for now just set Identity in this case. TODO
 	}
 
+	ROS_DEBUG_STREAM("H = " << H);
 	return H;
 }
 
