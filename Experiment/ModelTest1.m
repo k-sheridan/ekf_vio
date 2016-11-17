8fe5a0a69d048b78a036a7acfafcc27e71672098
Trexter <sheridak@purdue.edu>
2016-11-16 21:24:40 -0500

    finished the jacobean and state transition generation

--- Experiment/ModelTest1.m
+++ Experiment/ModelTest1.m
@@ -15,15 +15,15 @@ w_mag = sqrt((wx+bgx)^2 + (wy+bgy)^2 + (wz+bgz)^2);
 %syms w_mag
 
 %IMPORTANT--- if the omega values are 0 use this
-%dq0 = 1;
-%dq1 = 0;
-%dq2 = 0;
-%dq3 = 0;
+dq0 = 1;
+dq1 = 0;
+dq2 = 0;
+dq3 = 0;
 
-dq0 = cos(0.5 * w_mag * dt);
-dq1 = (2 * wx / w_mag) * sin(0.5 * w_mag * dt);
-dq2 = (2 * wy / w_mag) * sin(0.5 * w_mag * dt);
-dq3 = (2 * wz / w_mag) * sin(0.5 * w_mag * dt);
+%dq0 = cos(0.5 * w_mag * dt);
+%dq1 = (2 * wx / w_mag) * sin(0.5 * w_mag * dt);
+%dq2 = (2 * wy / w_mag) * sin(0.5 * w_mag * dt);
+%dq3 = (2 * wz / w_mag) * sin(0.5 * w_mag * dt);
 
 dq_mag = sqrt(dq0^2+dq1^2+dq2^2+dq3^2);
 dq0 = dq0 / dq_mag;
@@ -48,154 +48,145 @@ hx = [x + dx*dt + 0.5*newA(1)*dt*dt;
       bay;
       baz];
   
-hx = simplify(hx)
-
 
 
 syms s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11 s12 s13 s14 s15 s16 s17 s18 s19 s20
 syms s21 s22 s23 s24 s25 s26 s27 s28 s29 s30 s31 s32 s33 s34 s35 s36 s37 s38
 syms s39 s40 s41 s42 s43 s44 s45 s46 s47 s48 s49 s50
 syms s51 s52 s53 s54 s55 s56 s57 s58 s59 s60 s61 s62 s63 s64 s65 s66 s67 s68 s69 s70
-%H = simplify(subs(jacobian(hx, state), [bax;bay;baz;bgx;bgy;bgz], [0;0;0;0;0;0]));
-H = subs(H, [bax;bay;baz;bgx;bgy;bgz], [0;0;0;0;0;0]);
-[H, s1] = subexpr(H, s1)
+H = simplify(subs(jacobian(hx, state), [bax;bay;baz;bgx;bgy;bgz], [0;0;0;0;0;0]));
+%H = subs(H, [bax;bay;baz;bgx;bgy;bgz], [0;0;0;0;0;0]);
+[H, s1] = subexpr(H, s1);
 H = simplify(H);
-[H, s2] = subexpr(H, s2)
+[H, s2] = subexpr(H, s2);
 H = simplify(H);
-[H, s3] = subexpr(H, s3)
+[H, s3] = subexpr(H, s3);
 H = simplify(H);
-[H, s4] = subexpr(H, s4)
+[H, s4] = subexpr(H, s4);
 H = simplify(H);
-[H, s5] = subexpr(H, s5)
+[H, s5] = subexpr(H, s5);
 H = simplify(H);
-[H, s6] = subexpr(H, s6)
+[H, s6] = subexpr(H, s6);
 H = simplify(H);
-[H, s7] = subexpr(H, s7)
+[H, s7] = subexpr(H, s7);
 H = simplify(H);
-[H, s8] = subexpr(H, s8)
+[H, s8] = subexpr(H, s8);
 H = simplify(H);
-[H, s9] = subexpr(H, s9)
+[H, s9] = subexpr(H, s9);
 H = simplify(H);
-[H, s10] = subexpr(H, s10)
+[H, s10] = subexpr(H, s10);
 H = simplify(H);
-[H, s11] = subexpr(H, s11)
+[H, s11] = subexpr(H, s11);
 H = simplify(H);
-[H, s12] = subexpr(H, s12)
+[H, s12] = subexpr(H, s12);
 H = simplify(H);
-[H, s13] = subexpr(H, s13)
+[H, s13] = subexpr(H, s13);
 H = simplify(H);
-[H, s14] = subexpr(H, s14)
+[H, s14] = subexpr(H, s14);
 H = simplify(H);
-[H, s15] = subexpr(H, s15)
+[H, s15] = subexpr(H, s15);
 H = simplify(H);
-[H, s16] = subexpr(H, s16)
+[H, s16] = subexpr(H, s16);
 H = simplify(H);
-[H, s17] = subexpr(H, s17)
+[H, s17] = subexpr(H, s17);
 H = simplify(H);
-[H, s18] = subexpr(H, s18)
+[H, s18] = subexpr(H, s18);
 H = simplify(H);
-[H, s19] = subexpr(H, s19)
+[H, s19] = subexpr(H, s19);
 H = simplify(H);
-[H, s20] = subexpr(H, s20)
+[H, s20] = subexpr(H, s20);
 H = simplify(H);
-[H, s21] = subexpr(H, s21)
+[H, s21] = subexpr(H, s21);
 H = simplify(H);
-[H, s22] = subexpr(H, s22)
+[H, s22] = subexpr(H, s22);
 H = simplify(H);
-[H, s23] = subexpr(H, s23)
+[H, s23] = subexpr(H, s23);
 H = simplify(H);
-[H, s24] = subexpr(H, s24)
+[H, s24] = subexpr(H, s24);
 H = simplify(H);
-[H, s25] = subexpr(H, s25)
+[H, s25] = subexpr(H, s25);
 H = simplify(H);
-[H, s26] = subexpr(H, s26)
+[H, s26] = subexpr(H, s26);
 H = simplify(H);
-[H, s27] = subexpr(H, s27)
+[H, s27] = subexpr(H, s27);
 H = simplify(H);
-[H, s28] = subexpr(H, s28)
+[H, s28] = subexpr(H, s28);
 H = simplify(H);
-[H, s29] = subexpr(H, s29)
+[H, s29] = subexpr(H, s29);
 H = simplify(H);
-[H, s30] = subexpr(H, s30)
+[H, s30] = subexpr(H, s30);
 H = simplify(H);
-[H, s31] = subexpr(H, s31)
+[H, s31] = subexpr(H, s31);
 H = simplify(H);
-[H, s32] = subexpr(H, s32)
+[H, s32] = subexpr(H, s32);
 H = simplify(H);
-[H, s33] = subexpr(H, s33)
+[H, s33] = subexpr(H, s33);
 H = simplify(H);
-[H, s34] = subexpr(H, s34)
+[H, s34] = subexpr(H, s34);
 H = simplify(H);
-[H, s35] = subexpr(H, s35)
+[H, s35] = subexpr(H, s35);
 H = simplify(H);
-[H, s36] = subexpr(H, s36)
+[H, s36] = subexpr(H, s36);
 H = simplify(H);
-[H, s37] = subexpr(H, s37)
+[H, s37] = subexpr(H, s37);
 H = simplify(H);
-[H, s38] = subexpr(H, s38)
+[H, s38] = subexpr(H, s38);
 H = simplify(H);
-[H, s39] = subexpr(H, s39)
+[H, s39] = subexpr(H, s39);
 H = simplify(H);
-[H, s40] = subexpr(H, s40)
+[H, s40] = subexpr(H, s40);
 H = simplify(H);
-[H, s41] = subexpr(H, s41)
+[H, s41] = subexpr(H, s41);
 H = simplify(H);
-[H, s42] = subexpr(H, s42)
+[H, s42] = subexpr(H, s42);
 H = simplify(H);
-[H, s43] = subexpr(H, s43)
+[H, s43] = subexpr(H, s43);
 H = simplify(H);
-[H, s44] = subexpr(H, s44)
-H = simplify(H);
-[H, s45] = subexpr(H, s45)
-H = simplify(H);
-[H, s46] = subexpr(H, s46)
-H = simplify(H);
-[H, s47] = subexpr(H, s47)
-H = simplify(H);
-[H, s48] = subexpr(H, s48)
-H = simplify(H);
-[H, s49] = subexpr(H, s49)
-H = simplify(H);
-[H, s50] = subexpr(H, s50)
-H = simplify(H);
-[H, s51] = subexpr(H, s51)
-H = simplify(H);
-[H, s52] = subexpr(H, s52)
-H = simplify(H);
-[H, s53] = subexpr(H, s53)
-H = simplify(H);
-[H, s54] = subexpr(H, s54)
-H = simplify(H);
-[H, s55] = subexpr(H, s55)
-H = simplify(H);
-[H, s56] = subexpr(H, s56)
-H = simplify(H);
-[H, s57] = subexpr(H, s57)
-H = simplify(H);
-[H, s58] = subexpr(H, s58)
-H = simplify(H);
-[H, s59] = subexpr(H, s59)
-H = simplify(H);
-[H, s60] = subexpr(H, s60)
-H = simplify(H);
-[H, s61] = subexpr(H, s61)
-H = simplify(H);
-[H, s62] = subexpr(H, s62)
-H = simplify(H);
-[H, s63] = subexpr(H, s63)
-H = simplify(H);
-[H, s64] = subexpr(H, s64)
-H = simplify(H);
-[H, s65] = subexpr(H, s65)
-H = simplify(H);
-[H, s66] = subexpr(H, s66)
-H = simplify(H);
-[H, s67] = subexpr(H, s67)
-H = simplify(H);
-[H, s68] = subexpr(H, s68)
-H = simplify(H);
-[H, s69] = subexpr(H, s69)
-H = simplify(H);
-[H, s70] = subexpr(H, s70)
-H = simplify(H)
+
+H
+s1
+s2
+s3
+s4
+s5
+s6
+s7
+s8
+s9
+s10
+s11
+s12
+s13
+s14
+s15
+s16
+s17
+s18
+s19
+s20
+s21
+s22
+s23
+s24
+s25
+s26
+s27
+s28
+s29
+s30
+s31
+s32
+s33
+s34
+s35
+s37
+s38
+s39
+s40
+s41
+s42
+s43
+
+
+
 
--- Experiment/stateTransition.m
+++ Experiment/stateTransition.m
@@ -1,21 +1,30 @@
-function [newState] = stateTransition(state)
+function [newState] = stateTransition(state, measurement, dt)
     
+q = quatnormalize([state(7) state(8) state(9) state(10)]);
+
 x = state(1);
 y = state(2);
 z = state(3);
 dx = state(4);
 dy = state(5);
 dz = state(6);
-q0 = state(7);
-q1 = state(8);
-q2 = state(9);
-q3 = state(10);
+q0 = q(1);
+q1 = q(2);
+q2 = q(3);
+q3 = q(4);
 bax = state(11);
 bay = state(12);
 baz = state(13);
 bgx = state(14);
 bgy = state(15);
 bgz = state(16);
+
+ax = measurement(1);
+ay = measurement(2);
+az = measurement(3);
+wx = measurement(4);
+wy = measurement(5);
+wz = measurement(6);
 
 newState = [(((ay + bay)*(2*q0*q3 + 2*q1*q2))/2 - ((ax + bax)*(2*q2^2 + 2*q3^2 - 1))/2 - ((az + baz)*(2*q0*q2 - 2*q1*q3))/2)*dt^2 + dx*dt + x;
       (((az + baz)*(2*q0*q1 + 2*q2*q3))/2 - ((ax + bax)*(2*q0*q3 - 2*q1*q2))/2 - ((ay + bay)*(2*q1^2 + 2*q3^2 - 1))/2)*dt^2 + dy*dt + y;
