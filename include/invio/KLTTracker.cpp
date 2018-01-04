8b3b9a776dde4b8e09808ddea6540fa13d5e434a
Trexter <sheridak@purdue.edu>
2018-01-04 13:47:26 -0500

    started uncertainty aware klt tracker

--- include/invio/KLTTracker.cpp
+++ include/invio/KLTTracker.cpp
@@ -18,3 +18,13 @@ KLTTracker::~KLTTracker()
 	// TODO Auto-generated destructor stub
 }
 
+/*
+ * the main klt tracker function
+ * iteratively determines the new sub-pixel accurate feature positions in the new frame and estimates its covariance matrix
+ */
+void KLTTracker::findNewFeaturePositions(Frame& lf, Frame& cf, std::vector<Eigen::Vector2d>& previous_feature_positions,
+			std::list<Feature>& estimated_new_feature_positions, std::vector<Eigen::Vector2d>& measured_positions,
+			std::vector<Eigen::Matrix2d>& estimated_uncertainty, std::vector<bool>& passed)
+{
+
+}
--- include/invio/KLTTracker.h
+++ include/invio/KLTTracker.h
@@ -8,11 +8,23 @@
 #ifndef INVIO_INCLUDE_INVIO_KLTTRACKER_H_
 #define INVIO_INCLUDE_INVIO_KLTTRACKER_H_
 
+#include <Frame.h>
+#include <Feature.h>
+
+#include <Eigen/Core>
+
 class KLTTracker
 {
 public:
+
+
+
 	KLTTracker();
 	virtual ~KLTTracker();
+
+	void findNewFeaturePositions(const Frame& lf, const Frame& cf, const std::vector<Eigen::Vector2d>& previous_feature_positions,
+			const std::list<Feature>& estimated_new_feature_positions, std::vector<Eigen::Vector2d>& measured_positions,
+			std::vector<Eigen::Matrix2d>& estimated_uncertainty, std::vector<bool>& passed);
 };
 
 #endif /* INVIO_INCLUDE_INVIO_KLTTRACKER_H_ */
